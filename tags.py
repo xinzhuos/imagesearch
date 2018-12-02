92c279bae728a526e0493444edcfb3b185996c0a
SunXinzhuo <SunSara@SunXinzhuos-MacBook-Pro.local>
2018-12-02 00:38:03 -0500

    add reading feature csv files

--- tags.py
+++ tags.py
@@ -1,6 +1,8 @@
 
 import numpy as np
+import csv
 
+# Find set of supercategories, categories 
 supercategory_set = set()
 category_set = set()
 for i in range (10000):
@@ -12,12 +14,13 @@ for i in range (10000):
 		category_set.add(words[1])
 	file.close()
 
+# mapping from (super) category to index
 supercategory_dict = {item:val for val, item in enumerate(supercategory_set)}
 category_dict = {item:val+1 for val, item in enumerate(category_set)}
 
+# Vectorize tags
 train_tags = []
-
-for i in range (10):
+for i in range (10000):
 	file = open("tags_train/" + str(i) + ".txt", "r")
 	lines = file.readlines() 
 	row = np.zeros(len(supercategory_set))
@@ -29,4 +32,17 @@ for i in range (10):
 	train_tags.append(row)
 	file.close()
 
-print train_tags
+# Reading 1000-d train features
+features_train = np.zeros((10000,1000))
+with open('features_train/features_resnet1000_train.csv', 'r') as csvfile:
+	csv_reader = csv.reader(csvfile)
+	for line in csv_reader:
+		image_name = line[0].strip(".jpg")[13:]
+		row = []
+		for i in range(len(line)):
+			if i > 0:
+				row.append(float(line[i]))		
+		features_train[int(image_name)] = row
+
+
+
